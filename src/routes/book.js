219f84d0bcbd6bab6b8643f88e67d12f7271c09d
Anantshah133 <anantshah130305@gmail.com>
2024-03-08 23:08:21 +0530

    Data display in books table completed

--- src/routes/book.js
+++ src/routes/book.js
@@ -2,14 +2,18 @@ const express = require("express");
 const { Book } = require("../models/Book");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
+    res.render("books.ejs");
+});
+
+router.get("/get-books", async (req, res)=>{
     try {
         const books = await Book.find({});
-        res.render("books.ejs", books);
+        res.status(201).send(books);
     } catch (error) {
         console.log(`Error while fetching Books :- ${error}`);
     }
-});
+})
 
 router.get("/add", (req, res) => {
     res.render("addBook.ejs");
@@ -18,8 +22,6 @@ router.get("/add", (req, res) => {
 router.post("/add", async (req, res) => {
     try {
         const { title, author, category, quantity, price, publish_date } = req.body;
-        // console.log(publish_date);
-        // const parsedDate = new Date(publish_date);
         const book = await Book.create({
             title,
             author,
