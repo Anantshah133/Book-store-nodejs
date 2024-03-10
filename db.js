02d86d5d6e52be8243ae7895cc4ee6497d761103
Anantshah133 <anantshah130305@gmail.com>
2024-03-10 23:11:14 +0530

    Nothing just changes terminal text color

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { mongoURL } = require("./src/config/config");
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(mongoURL);
-        console.log(`Mongodb connection successful : ${conn.connection.host}`.green.inverse);
+        console.log(`Mongodb connection successful : ${conn.connection.host}`.green);
     } catch (error) {
         console.log(`Mongodb connection failed - ${error}`);
     }
--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static("./public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/books", bookRoute);
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT :- ${PORT}`.green.bold);
+    console.log(`Click here to open in browser : `.green + `http://localhost:${PORT}`.red.bold);
 })
